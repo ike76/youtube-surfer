69b2cffc3862b8f61ffa0e92d48c4e9edec97847
ike76 <ike76@me.com>
2018-01-23 15:09:28 -0600

    working model

--- index.js
+++ index.js
@@ -1,52 +1,86 @@
+$(function(){
 
-const endpointUrl = 'https://www.googleapis.com/youtube/v3/search'
-const devKey = 'AIzaSyC7KpQEBzM-kblGHbn9-FSlwEpmJnnZoI8';
+function hideResults(){
+	$('.results').hide();
+}
 
-function renderResult(result) {
-	console.log('render result was called')
+function renderResult (item){
 	return `<div class="result">
-		<a href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank">
-			<img src="${result.snippet.thumbnails.medium.url}" alt="alt text">
-			<p>${result.snippet.title}</p>
-		</a>
-	</div>`
+				<a href="https://www.youtube.com/watch?v=${item.id.videoId}" target="_blank">
+					<img src="${item.snippet.thumbnails.medium.url}" alt="alt text">
+					<p>${item.snippet.title.substring(0,60)}</p>
+				</a>
+					
+				<div class="more-channel-link"><p>More from <strong><a href="https://www.youtube.com/channel/${item.snippet.channelId}" target="_blank">${item.snippet.channelTitle}</a></strong></p></div>
+			</div>`
+
+	// return `<div class="result">
+	// 			<a href="https://www.youtube.com/watch?v=${item.id.videoId}" target="_blank">
+	// 				<div class="thumbnail" css('background-image', url("${item.snippet.thumbnails.medium.url}"))>
+						
+	// 				</div>
+	// 				<p>${item.snippet.title}</p>
+	// 			</a>
+	// 		</div>`
 }
 
-function watchSubmit(){
-	// set listener for input form and put search query into searchQuery variable
-	$('.js-search-form').on('submit', event => {
+let query = '';
+
+function handleSubmit(){
+	$('.js-search-form').on('submit', function(event) {
 		event.preventDefault();
-		const queryTarget = $(event.currentTarget).find('.js-query');
-		const query = queryTarget.val()
-		// clear searchbox
-		queryTarget.val('')
-		getDataFromApi(query, displayYouTubeResults);
-
+		const queryBox = $(this).find('.js-query');
+		query = queryBox.val();
+		//empty the box
+		queryBox.val('')
+		callGoogle(query, displayResults)
+		$('.results').show();
 	})
-	// call google to get json object back
 }
 
-
-function getDataFromApi(searchQuery, callback){
-	const querySettings = {
-		q: searchQuery,
+function callGoogle(query, callback, pageToken = ''){
+	const endpoint = 'https://www.googleapis.com/youtube/v3/search';
+	const queryObject = {
+		maxResults: 6,
+		part: 'snippet',
 		key: 'AIzaSyC7KpQEBzM-kblGHbn9-FSlwEpmJnnZoI8',
-		part: 'snippet'
-	}
-	$.getJSON(endpointUrl, querySettings, callback);
-}
+		q: query,
+		pageToken: pageToken,
+	};
+	$.getJSON(endpoint, queryObject, callback)
+};
 
-
-
-function displayYouTubeResults(data){
-	const results = data.items.map(item => {
+function displayResults(data){
+	const results = data.items.map(item=>{
 		return renderResult(item);
 	});
-	$('.js-search-results').html(results);
+	setButtonTokens(data);
+	$('.js-search-results').html(results)
 }
 
+function setButtonTokens(data){
+	let {prevPageToken = '', nextPageToken = ''} = data;
+	$('.btn').removeClass('disabled')
+	if (prevPageToken === '') $('#prev-button').addClass('disabled');
+	if (nextPageToken === '') $('#next-button').addClass('disabled');
+	$('#prev-button').attr({
+		token: prevPageToken,
+	});
+	$('#next-button').attr({
+		token: nextPageToken,
+	});
+}
+
+// button listeners
+$('.nav-buttons button').on('click', function(event){
+	let token = $(this).attr('token');
+	console.log(token);
+	callGoogle(query, displayResults, token);
+})
+
+handleSubmit();
+hideResults();
 
 
-// get the json object from google api
 
-$(watchSubmit);
+})
